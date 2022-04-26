782a1f56f4eabc3cea3bdbea217c3b5621eca01f
sjh970603 <sjh970603@naver.com>
2022-04-26 22:03:59 +0900

    Yap

---
+++ react/react-native-calendars-master/example/src/screens/Loginpage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loginp.css'
+
+
+function App() {
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <div>개발 블로그</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ react/react-native-calendars-master/example/src/screens/index.ts
@@ -0,0 +1,22 @@
+import {Navigation} from 'react-native-navigation';
+
+import MenuScreen from './menu';
+import CalendarsScreen from './calendars';
+import AgendaScreen from './agenda';
+import CalendarsList from './calendarsList';
+import HorizontalCalendarList from './horizontalCalendarList';
+import ExpandableCalendar from './expandableCalendar';
+import TimelineCalendar from './timelineCalendar';
+import LoginPage from './Loginpage';
+
+
+export function registerScreens() {
+  Navigation.registerComponent('Menu', () => MenuScreen);
+  Navigation.registerComponent('Calendars', () => CalendarsScreen);
+  Navigation.registerComponent('Agenda', () => AgendaScreen);
+  Navigation.registerComponent('CalendarsList', () => CalendarsList);
+  Navigation.registerComponent('HorizontalCalendarList', () => HorizontalCalendarList);
+  Navigation.registerComponent('ExpandableCalendar', () => ExpandableCalendar);
+  Navigation.registerComponent('TimelineCalendar', () => TimelineCalendar);
+  Navigation.registerComponent('Loginpage', () => LoginPage);
+}
